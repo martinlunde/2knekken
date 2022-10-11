e197dace92a5944f5cbdc1d17644abfb4e031fb9
Bjørn-Olav Strand <bolav@hyre.no>
2022-10-11 21:15:47 +0200

    :sparkles: bare bones lunsj support:

--- backend/index.ts
+++ backend/index.ts
@@ -8,8 +8,27 @@ const app: Express = express();
 const port = process.env.PORT;
 
 app.use('/xkcd', proxy('https://xkcd.com'));
+// app.use('/lunsj', proxy('http://api.e24.no'))
+// http://api.e24.no/content/v1/comics/2022-10-10
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
+});
+
+app.get('/lunsj/*', (req: Request, res: Response) => {
+    console.log(req.path);
+    const date_or_number = req.path.split('/')[2];
+    console.log(date_or_number);
+    let date_str = date_or_number;
+    try {
+        const no = parseInt(date_or_number);
+        console.log(`convert to datestring ${no}`);
+    }
+    catch (e) {
+        // Do nothing
+    }
+    const date_parts = date_str.split('-');
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ "date": date_str, "month": date_parts[1], "num": 1500, "link": "", "year": date_parts[0], "news": "", "safe_title": "", "transcript": "", "img": `http://api.e24.no/content/v1/comics/${date_str}`, "title": `Lunsj for ${date_str}`, "day": date_parts[2] }));
 });
 
 app.listen(port, () => {
