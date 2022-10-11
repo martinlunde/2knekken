5fe782973182c7f5f88fcea023676c43e68b9c60
MartinLunde <martinlunde96@gmail.com>
2022-10-11 19:01:16 +0200

    Add react-query and setup structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,24 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import DashboardView from './views/DashboardView';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='text-teal-600'>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DashboardView />
   );
 }
 
