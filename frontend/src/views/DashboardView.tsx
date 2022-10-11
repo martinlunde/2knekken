82908c5a57989ed9ebfd96c626ddfc14d7a107d7
MartinLunde <martinlunde96@gmail.com>
2022-10-11 19:44:39 +0200

    Show xkcd image

--- frontend/src/views/DashboardView.tsx
+++ frontend/src/views/DashboardView.tsx
@@ -1,17 +1,33 @@
 import React from "react";
 import { useQuery } from "react-query";
 
+interface ComicResponse {
+  day: string;
+  month: string;
+  year: string;
+  num: string;
+  link: string;
+  news: string;
+  safe_title: string;
+  transcript: string;
+  alt: string;
+  img: string;
+  title: string;
+}
+
 export default function DashboardView() {
-  async function getComic() {
+
+  async function getComic(): Promise<ComicResponse> {
     const result = await fetch("2680/info.0.json");
     return result.json();
   }
 
-  const { data } = useQuery(["2680"], () => getComic(), { retry: false });
+  const { data: comic } = useQuery(["2680"], () => getComic(), { retry: false });
 
   return (
-    <div>
-      <h1>{data?.alt}</h1>
+    <div className="flex flex-1 flex-col justify-center">
+      <h1>{comic?.alt}</h1>
+      <img className="w-96" src={comic?.img} alt={comic?.alt} />
     </div>
   );
 }
